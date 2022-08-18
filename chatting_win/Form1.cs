47cf41aefe114ea0270bc803cc76d5dd7293c918
gaebal-agma <cw012478@gmail.com>
2022-08-18 16:57:50 +0900

    first commit

---
+++ chatting_win/Form1.cs
@@ -0,0 +1,15 @@
+namespace chatting_win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+    }
+}
